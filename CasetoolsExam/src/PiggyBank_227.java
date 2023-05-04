bc0c3a08efb5ea871272e30427a8c60ddeddca53
exam <exam@Bcalab4-m04>
2023-05-04 12:16:15 +0530

    First commit.

---
+++ CasetoolsExam/src/PiggyBank_227.java
@@ -0,0 +1,23 @@
+
+public class PiggyBank_227 {
+	
+	int num_coins;
+	int coin_value;
+	
+	PiggyBank_227()
+	{
+		this.num_coins=num_coins;
+	}
+		
+
+	public static void main(String[] args) {
+		
+		
+		
+			
+		}
+		// TODO Auto-generated method stub
+
+	
+
+}
